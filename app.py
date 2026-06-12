36f5a933b7017c9cb86ba302b4bedd5343ab2f90
vaidehikh-2028 <24UCS289@lnmiit.ac.in>
2026-06-12 21:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import wordpunct_tokenize
 
-nltk.download('punkt_tab')
-nltk.download('punkt')
+# Download only stopwords
 nltk.download('stopwords')
 
 ps = PorterStemmer()
@@ -18,12 +18,13 @@ tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 
 def transform_text(text):
     text = text.lower()
-    from nltk.tokenize import wordpunct_tokenize
 
-text = wordpunct_tokenize(text)
+    # Tokenize text
+    text = wordpunct_tokenize(text)
 
     y = []
 
+    # Keep alphanumeric words
     for i in text:
         if i.isalnum():
             y.append(i)
@@ -31,6 +32,7 @@ text = wordpunct_tokenize(text)
     text = y[:]
     y.clear()
 
+    # Remove stopwords and punctuation
     for i in text:
         if i not in stop_words and i not in string.punctuation:
             y.append(i)
@@ -38,6 +40,7 @@ text = wordpunct_tokenize(text)
     text = y[:]
     y.clear()
 
+    # Stemming
     for i in text:
         y.append(ps.stem(i))
 
@@ -48,6 +51,7 @@ st.title("📩 Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter your message")
 
 if st.button("Predict"):
+
     transformed_sms = transform_text(input_sms)
 
     vector_input = tfidf.transform([transformed_sms])
